f9792130ba8cbaa5af5bae2e31e16461c3c4ad2c
Capperinger <fox4evergame@gmail.com>
2025-12-11 19:36:11 -0500

    create extend and retract command that can be logged.

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -10,12 +10,31 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
+
+  public Command extendClimberCommand(Climber subsystem) {
+    if (!subsystem.getTopLimit()) {
+      return Commands.startEnd(() -> subsystem.setSpeed(0.5), () -> subsystem.stop()).until(() -> subsystem.getTopLimit()).withName("Extend Climber");
+    } else{
+      return Commands.none();
+    }
+  }
+
+  public Command retractClimberCommand(Climber subsystem) {
+    if (!subsystem.getBottomLimit()) {
+      return Commands.startEnd(() -> subsystem.setSpeed(-0.5), () -> subsystem.stop()).until(() -> subsystem.getBottomLimit()).withName("Retract Climber");
+    } else{
+      return Commands.none();
+    }
+  }
+
   private final SparkMax m_motor = new SparkMax(1, MotorType.kBrushless);
-  
+
   private final DigitalInput m_topLimit = new DigitalInput(1);
   private final DigitalInput m_bottomLimit = new DigitalInput(2);
   private final RelativeEncoder m_encoder = m_motor.getEncoder();
@@ -23,39 +42,42 @@ public class Climber extends SubsystemBase {
   private SparkMaxConfig m_leftMotorConfig;
   private boolean m_isRunning = false;
 
-  public Climber() {}
+  public Climber() {
+  }
 
-  /*public enum ClimberState{
-    CLIMBER_UP,
-    CLIMBER_DOWN,
-    CLIMBER_STOP
-  }*/
+  /*
+   * public enum ClimberState{
+   * CLIMBER_UP,
+   * CLIMBER_DOWN,
+   * CLIMBER_STOP
+   * }
+   */
 
   public void stop() {
     m_motor.set(0);
   }
 
   // public void extendClimber() { //ExtendClimber Idea
-  //   if(m_topLimit.get()) {
-  //     m_motor.set(0);
-  //   } else{
-  //     m_motor.set(0.5);
-  //   }
+  // if(m_topLimit.get()) {
+  // m_motor.set(0);
+  // } else{
+  // m_motor.set(0.5);
+  // }
   // }
 
   // public void retractClimber() { //RetractClimber Idea
-  //   if(m_bottomLimit.get()) {
-  //     m_motor.set(0);
-  //   } else {
-  //     m_motor.set(-0.5);
-  //   }
+  // if(m_bottomLimit.get()) {
+  // m_motor.set(0);
+  // } else {
+  // m_motor.set(-0.5);
+  // }
   // }
 
-  public void setSpeed(double speed) { //setSpeed Idea
+  public void setSpeed(double speed) { // setSpeed Idea
     m_motor.set(speed);
   }
 
-  public boolean getTopLimit(){
+  public boolean getTopLimit() {
     return m_topLimit.get();
   }
 
@@ -63,11 +85,11 @@ public class Climber extends SubsystemBase {
     return m_bottomLimit.get();
   }
 
-  public double getVelocity(){
+  public double getVelocity() {
     return m_encoder.getVelocity();
   }
 
-  public double getPosition(){
+  public double getPosition() {
     return m_encoder.getPosition();
   }
 
