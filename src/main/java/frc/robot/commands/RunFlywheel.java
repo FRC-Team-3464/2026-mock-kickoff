10e172ccc82ed175d9e4e7fa25a4f446684624b7
daniel <dan.senchukov@gmail.com>
2025-12-16 17:58:54 -0500

    Changed "setIntakeMotorSpeed" to "setFlywheelMotorSpeed"

--- src/main/java/frc/robot/commands/RunFlywheel.java
+++ src/main/java/frc/robot/commands/RunFlywheel.java
@@ -21,12 +21,12 @@ public class RunFlywheel extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {} 
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_Subsystem.setIntakeMotorSpeed(m_Speed);
+    m_Subsystem.setFlywheelMotorSpeed(m_Speed);
   }
 
   // Called once the command ends or is interrupted.
