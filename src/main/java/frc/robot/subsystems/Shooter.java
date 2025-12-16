27349c230c4c79ca9b35abb3c1f90ff3979281c9
sam <omada916@gmail.com>
2025-12-16 17:55:22 -0500

    line commands

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -13,10 +13,31 @@ import edu.wpi.first.wpilibj.DigitalInput;
 
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
+  public Command LowerPivot() {
+    if (!getLowMotor()) {
+      return Commands.startEnd(()-> setPivotSpeed(-0.5),() -> stopPivot()).until(() -> getLowMotor()).withName("Lower pivot");
+    } else {
+      return Commands.none();
+    }
+  }
+  public Command RaisePivot() {
+    if (!getHighMotor()) {
+      return Commands.startEnd(() -> setPivotSpeed(0.5),() -> stopPivot()).until(() -> getHighMotor()).withName("Raise pivot");
+    } else {
+      return Commands.none();
+    }
+  }
+  public Command RunFlywheel() {
+    return Commands.startEnd(() -> setFlywheelMotorSpeed(0.5), () -> stopFlywheel()).withName("Run Flywheel");
+  }
+  public Command RunIntake() {
+    return Commands.startEnd(() -> setIntakeMotorSpeed(0.5),() -> stopIntakeMotor()).withName("Run Intake");
+  }
   /** Creates a new Shooter. */ 
   public final SparkMax m_FlywheelMotor = new SparkMax(7, MotorType.kBrushless);
   public final SparkMax m_PivotMotor = new SparkMax(8, MotorType.kBrushless);
@@ -28,11 +49,11 @@ public class Shooter extends SubsystemBase {
   public RelativeEncoder m_intakeMotorEncoder = m_intakeMotor.getEncoder();
   public RelativeEncoder m_FlywheelMotorEncoder = m_FlywheelMotor.getEncoder();
   public RelativeEncoder m_PivotMotorEncoder = m_PivotMotor.getEncoder();
-  public final DigitalInput m_minimumPivotSwitch = new DigitalInput(10);
-  public final DigitalInput m_maximumPivotSwitch = new DigitalInput(11);
+
   private boolean m_isFlywheelRunning = false;
-  private boolean m_ispivotMotorRunning = false;
-  private boolean m_isintakeMotorRunning = false;
+  private boolean m_isPivotMotorRunning = false;
+  private boolean m_isIntakeMotorRunning = false;
+
   public SparkMaxConfig m_intakeMotorConfig;
   public SparkMaxConfig m_PivotMotorConfig;
   public SparkMaxConfig m_FlywheelMotorConfig;
@@ -47,12 +68,19 @@ public class Shooter extends SubsystemBase {
     m_intakeMotor.configure(m_intakeMotorConfig, null, null);
   } // floyd
 
+  public boolean getHighMotor() {
+    return m_topLimitSwitch.get();
+  }
+  public boolean getLowMotor() {
+    return m_bottomLimitSwitch.get();
+  }
   public void setPivotSpeed(double speed){
-    m_ispivotMotorRunning = speed!= 0;
+    m_isPivotMotorRunning = speed!= 0;
     m_PivotMotor.set(speed);
   }
 
-  public void stopPivot(double speed) {
+  public void stopPivot() {
+    m_isPivotMotorRunning = false;
     m_PivotMotor.set(0);
   }
 
@@ -63,6 +91,9 @@ public class Shooter extends SubsystemBase {
 
   public void stopIntakeMotor() {
     m_intakeMotor.set(0);
+  }
+  public void stopFlywheel() {
+    m_FlywheelMotor.set(0);
   }
   public void setFlywheelMotorSpeed(double speed) {
     m_isFlywheelRunning = speed != 0;
