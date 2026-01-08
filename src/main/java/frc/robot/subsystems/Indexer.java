97f3b9d62d9f8543c93a7724810e2a84bbc6a47b
Owen Biamonte <29biamono@simsburyschools.net>
2026-01-08 18:08:12 -0500

    added triggers for motor position to determine the number of balls in the indexer, added javadocs, and fixed minor syntax bugs

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -2,12 +2,17 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
+
+
+
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
 
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -17,7 +22,17 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
+/**
+ * This subsystem controls the indexer for the robot.
+ * It counts the number of balls taken in,
+ * and can set the speed and find the position and speed of the motor.
+ * It does this so that the balls taken in can be counted and then
+ * stored until they are shot.
+ * 
+ * @author Matthew McGrath
+ * @author Ryan Hefferon
+ * @author Owen Biamonte
+ */
 public class Indexer extends SubsystemBase {
 
   public double rotations;
@@ -34,19 +49,38 @@ public class Indexer extends SubsystemBase {
     m_rightMotor.configure(rightMotorConfig, null, null);
   }
 
+  /**checks to see how much the indexer has rotated to determine if there are 5 balls*/
+  Trigger full = new Trigger(m_encoder.getPosition()%1 == 5);
+  /**checks to see how much the indexer has rotated to determine if there are 4 balls*/
+  Trigger fourFull = new Trigger(m_encoder.getPosition()%1 == 4);
+  /**checks to see how much the indexer has rotated to determine if there are 3 balls*/
+  Trigger threeFull = new Trigger(m_encoder.getPosition()%1 == 3);
+  /**checks to see how much the indexer has rotated to determine if there are 2 balls*/
+  Trigger twoFull = new Trigger(m_encoder.getPosition()%1 == 2);
+  /**checks to see how much the indexer has rotated to determine if there are 1 balls*/
+  Trigger oneFull = new Trigger(m_encoder.getPosition()%1 == 1);
+  /**checks to see how much the indexer has rotated to determine if there are 0 balls*/
+  Trigger empty = new Trigger(m_encoder.getPosition()%1 == 0);
+
+  /**sets the speed of the indexer motor
+   * @param speed the speed to run the motor at (-1.0 to 1.0)
+  */
   public void setSpeed(double speed) {
     m_leftMotor.set(speed);
-
   }
-
+  /**stops the speed*/
   public void stop() {
     m_leftMotor.set(0);
   }
-
+  /**finds the speed of the indexer motor
+   * @return motor velocity in RPM
+  */
   public double getVelocity() {
     return m_encoder.getVelocity();
   }
-
+  /**finds the number of position of the indexer motor
+   * @return motor rotations
+   */
   public double getPosition() {
     return m_encoder.getPosition();
   }
@@ -56,13 +90,13 @@ public class Indexer extends SubsystemBase {
         () -> setSpeed(speed),
         () -> stop()).withName("Spin Indexer");
   }
-
+  
   public Command rotate() {
     double rotations = getPosition();
     // Use .until() after the spin until the difference in position is at least 72
     return spin(0.5)
-    .until(double rotations >= 72);
-    .withName("Rotate Indexer");
+    .until(rotations >= 72);
+    withName("Rotate Indexer");
   }
 
   public void JoystickButton() {
